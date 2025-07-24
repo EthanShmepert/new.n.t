4cdc714268f585813ba01bce734c2e4a18d012dd
hadeyemo <133528746+hadeyemo@users.noreply.github.com>
2025-07-24 18:05:06 -0500

    the changes to my levels
    I was able to make lvel 1 and 4 redirect from the map but, i was not able to make it so that when the player finished they go to the map

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 func _process(delta: float) -> void:
-	if $"..".position.y > 200:
-		position_smoothing_speed = 0
-	elif position_smoothing_speed != 10:
-		position_smoothing_speed = 10
+    if $"..".position.y > 200:
+        position_smoothing_speed = 0
+    elif position_smoothing_speed != 10:
+        position_smoothing_speed = 10
