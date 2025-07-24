01ef23fd9784f1f8a551fdfdef1d32c441eb31d9
hadeyemo <133528746+hadeyemo@users.noreply.github.com>
2025-07-24 02:11:25 -0500

    Levell1 and 4
    I think i was able  to make level 1 and 4 follow the same path and i added it to the main menu

--- levels Page/new.n.t-main/Scripts/Camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _process(delta: float) -> void:
-	if $"..".position.y > 200:
-		position_smoothing_speed = 0
-	elif position_smoothing_speed != 10:
-		position_smoothing_speed = 10
--- levels Page/new.n.t-main/Scripts/mapCamera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _ready():
-	var cam = $"."
-	cam.make_current()
-	cam.zoom = Vector2(.79, .79)  
-	
