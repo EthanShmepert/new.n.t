2f1f731630e814832317569b06128db943d59d83
SebMares <91852663+SebMares@users.noreply.github.com>
2025-07-23 22:03:47 -0500

    Added Level 2 Scene and scripts

---
+++ pencil.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+@export var direction: Vector2 = Vector2.LEFT
+
+func _ready():
+	velocity = direction.normalized() * speed
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		on_hit(collision)
+
+func on_hit(collision: KinematicCollision2D):
+	var collider = collision.get_collider()
+	if collider.is_in_group("enemies"):
+		# Call a damage method if available
+		if collider.has_method("take_damage"):
+			collider.take_damage()
+	queue_free()
